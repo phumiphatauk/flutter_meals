6fd073373da2d2806b8b94676a3393f25a8a66ab
phumiphat <phumiphat.auk@gmail.com>
2025-05-30 15:23:45 +0700

    Implement tabs for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals/screens/categories.dart';
-import 'package:flutter_meals/screens/meals.dart';
-import 'package:flutter_meals/data/dummy_data.dart';
+import 'package:flutter_meals/screens/tabs.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
@@ -22,6 +20,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const CategoriesScreen());
+    return MaterialApp(theme: theme, home: const TabsScreen());
   }
 }
